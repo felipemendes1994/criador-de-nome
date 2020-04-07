0e6c1c2d796bb365bd97ff5274bbe79855eac9ac
Felipe Mendes <felipemendes1994@gmail.com>
2020-04-06 22:19:55 -0300

    Refatorado codigo para componentizacao

--- src/App.vue
+++ src/App.vue
@@ -6,81 +6,7 @@
 			<h6 class="text-secondary">Gerador de nomes utilizando Vue.js</h6>
 		</div>
 		<div id="main">
-			<div class="container">
-				<div class="row">
-					<div class="col-md">
-						<h5>Prefixos <span class="badge badge-info">{{ prefixes.length }}</span></h5>
-						<div class="card">
-							<div class="card-body">
-								<ul class="list-group">
-									<li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-										<div class="row">
-											<div class="col-md">{{ prefix }}</div>
-											<div class="col-md text-right">
-												<button class="btn btn-info" v-on:click="deletePrefix(prefix)"><span
-														class="fa fa-trash"></span></button>
-											</div>
-										</div>
-									</li>
-								</ul>
-								<br />
-								<div class="input-group">
-									<input type="text" class="form-control" placeholder="Digite o prefixo" v-model="prefix"
-										v-on:keyup.enter="addPrefix(prefix)" />
-									<div class="input-group-append">
-										<button class="btn btn-info" v-on:click="addPrefix(prefix)"> <span
-												class="fa fa-plus"></span></button>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md">
-						<h5>Sufixos<span class="badge badge-info">{{ sufixes.length }}</span></h5>
-						<div class="card">
-							<div class="card-body">
-								<ul class="list-group">
-									<li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
-										<div class="row">
-											<div class="col-md">{{ sufix }}</div>
-											<div class="col-md text-right">
-												<button class="btn btn-info" v-on:click="deleteSufix(sufix)"><span
-														class="fa fa-trash"></span></button>
-											</div>
-										</div>
-									</li>
-								</ul>
-								<br />
-								<div class="input-group">
-									<input type="text" class="form-control" placeholder="Digite o sufixo" v-model="sufix"
-										v-on:keyup.enter="addSufix(sufix)" />
-									<div class="input-group-append">
-										<button class="btn btn-info" v-on:click="addSufix(sufix)"><span
-												class="fa fa-plus"></span></button>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<br />
-				<h5>Domains<span class="badge badge-info">{{ domains.length }}</span></h5>
-				<div class="card">
-					<div class="card-body">
-						<ul class="list-group">
-							<li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
-								<div class="row">
-									<div class="col-md">{{ domain.name }}</div>
-									<div class="col-md text-right">
-										<a class="btn btn-info" v-bind:href="domain.checkout" target="_blank"><span
-												class="fa fa-shopping-cart"></span></a>
-									</div>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
+			<DomainList></DomainList>
 		</div>
 	</div>
 </template>
@@ -88,52 +14,12 @@
 <script>
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import DomainList from "./components/DomainList";
 
 export default {
 	name: "App",
-	data: function() {
-		return {
-			prefix: "",
-			sufix: "",
-			prefixes: ["Air", "Jet", "Flight"],
-			sufixes: ["Hub", "Station", "Mart"]
-		};
-	},
-	methods: {
-		addPrefix(prefix){
-			this.prefixes.push(prefix);
-			this.prefix = "";
-			this.generateDomains();
-		},
-		deletePrefix(prefix){
-			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
-			this.generateDomains();
-		},
-		addSufix(sufix){
-			this.sufixes.push(sufix);
-			this.sufix = "";
-			this.generateDomains();
-		},
-		deleteSufix(sufix){
-			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
-			this.generateDomains();
-		}
-	},
-	computed: {
-		domains() {
-			const domains = [];
-			for(const prefix of this.prefixes){
-				for(const sufix of this.sufixes){
-					const name = prefix + sufix;
-					const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${name.toLowerCase()}&tld=.com.br`;
-					domains.push({
-						name,
-						checkout
-					});
-				}
-			}
-			return domains;
-		}
+	components: {
+		DomainList
 	}
 };
 
